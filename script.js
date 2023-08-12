8d284081a6b5600541bb4a29d0f613e0ed8a4831
vishnu-sainiii <93662449+vishnu-sainiii@users.noreply.github.com>
2023-08-12 21:09:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ['Front-end Developer', 'Web-Developer ', 'Geo-Political Analyzer :)'],
+    typeSpeed: 50,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true
+  });
